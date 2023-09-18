a030704136258fff9806cdbf6a98bf3fc12b147e
Lukas Osadsky <79591710+lukas-osadsky-swi@users.noreply.github.com>
2023-09-18 17:00:42 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,7 +16,7 @@ wget "https://github.com/TheLocehiliosan/yadm/raw/master/yadm" -qO $YADM
 chmod 755 $YADM
 
 # clone dotfiles
-$YADM clone https://github.com/jiriskacel/dotfiles -b yadm/linux --no-bootstrap
+$YADM clone https://github.com/lukas-osadsky-swi/dotfiles -b yadm/linux --no-bootstrap
 $YADM checkout HEAD -- $HOME
 for class in $CLASS; do
     $YADM config --add local.class $class
