53158990f1a180da7b57c091cfa107a981ffe82e
Jiri Skacel <jiri.skacel@seznam.cz>
2024-01-05 20:56:25 +0100

    add class to bootstraps

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,8 +1,12 @@
+param(
+    [string[]]$Class = @()
+)
+
 $YADM = "$env:USERPROFILE\bin\yadm"
 $YADM_BAT = "$YADM.bat"
 
 # install yadm
-New-Item -ItemType Directory (Split-Path -Parent $YADM) | Out-Null
+New-Item -ItemType Directory (Split-Path -Parent $YADM) -EA 0 | Out-Null
 Invoke-WebRequest "https://github.com/TheLocehiliosan/yadm/raw/master/yadm" -OutFile $YADM
 @'
 @echo off
@@ -16,4 +20,8 @@ set ShellExe=%GitFolder%bin\sh.exe
 # clone dotfiles
 & $YADM_BAT clone "https://github.com/jiriskacel/dotfiles" -b "yadm/windows" --no-bootstrap
 & $YADM_BAT checkout HEAD -- $HOME
+$Class | ForEach-Object {
+    & $YADM_BAT config --add local.class $_
+}
+& $YADM_BAT alt
 & $YADM_BAT bootstrap
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 YADM=$HOME/bin/yadm
 
+while getopts c: name
+do
+    case $name in
+    c)    CLASS="$CLASS $OPTARG";;
+    ?)   printf "Usage: %s: [-c class]\n" $0
+          exit 2;;
+    esac
+done
+
 # install yadm
 mkdir -p $HOME/bin
 wget "https://github.com/TheLocehiliosan/yadm/raw/master/yadm" -qO $YADM
@@ -9,4 +18,8 @@ chmod 755 $YADM
 # clone dotfiles
 $YADM clone https://github.com/jiriskacel/dotfiles -b yadm/linux --no-bootstrap
 $YADM checkout HEAD -- $HOME
+for class in $CLASS; do
+    $YADM config --add local.class $class
+done
+$YADM alt
 $YADM bootstrap
